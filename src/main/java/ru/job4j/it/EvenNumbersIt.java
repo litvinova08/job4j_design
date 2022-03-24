84b47079aba320099eae773994a701bbd8d24177
litvinova08 <ma.litvinova08@gmail.com>
2022-03-24 21:39:48 +1100

    5.1.2. Создать итератор четные числа [#150 #139900] V2

--- src/main/java/ru/job4j/it/EvenNumbersIt.java
+++ src/main/java/ru/job4j/it/EvenNumbersIt.java
@@ -3,38 +3,54 @@ package ru.job4j.it;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * @author Margarita Litvinova
+ * @version 1.0
+ */
 public class EvenNumbersIt implements Iterator<Integer> {
+    /**
+     * index - это счетчик итератора
+     */
     private int[] data;
     private int index;
 
+    /**
+     * метод принимает массив для последующего использования итератора
+     * @param data массив, из которого будут отобраны четные числа
+     */
     public EvenNumbersIt(int[] data) {
         this.data = data;
     }
 
+    /**
+     * метод позволяет определить, есть ли еще елементы в массиве
+     * @return true если в массиве еще есть четные элементы
+     */
     @Override
     public boolean hasNext() {
-        return evenExist() != -1;
-    }
-
-    @Override
-    public Integer next() {
-        int rsl = evenExist();
-        if (rsl == -1) {
-            throw new NoSuchElementException();
-        } else {
-            index++;
-            return rsl;
-        }
-    }
-
-    public int evenExist() {
-        int rsl = -1;
+        boolean rsl = false;
         for (int i = index; i < data.length; i++) {
             if (data[i] % 2 == 0) {
-                rsl = data[i];
                 index = i;
+                rsl = true;
                 break;
             }
         } return rsl;
+    }
+
+    /**
+     * метдо возвращает следующий четный элемент
+     * @return следующий четный элемент или -1, если нет больше четных элементов
+     */
+    @Override
+    public Integer next() {
+        int rsl;
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        } else {
+            rsl = data[index];
+            index++;
+            return rsl;
+        }
     }
 }
