871ea9ab8e0ed1b76cc8013e58f5ecaaac3150b4
litvinova08 <ma.litvinova08@gmail.com>
2022-03-31 23:34:40 +1100

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -0,0 +1,65 @@
+package ru.job4j.it;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.next();
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+                iterator.add(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T temp = iterator.next();
+            for (T tempElement : elements) {
+                if (temp.equals(tempElement)) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+}
